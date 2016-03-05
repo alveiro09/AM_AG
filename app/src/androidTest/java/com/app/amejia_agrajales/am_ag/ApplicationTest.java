1e34f55f33534b3797065c789f6a27c4c8f35255
Alveiro Mejia <alveiro09@hotmail.com>
2016-03-05 15:52:08 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/app/amejia_agrajales/am_ag/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.app.amejia_agrajales.am_ag;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/app/amejia_agrajales/am_ag/actIngresosMensuales.java
@@ -0,0 +1,12 @@
+package com.app.amejia_agrajales.am_ag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class actIngresosMensuales extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lytingresosmensuales);
+    }
+}
